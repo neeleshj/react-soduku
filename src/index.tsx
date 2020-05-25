60272de8d484adeefa0e41cb5ae2a968b4c2a8f4
Neelesh Joshi <neeleshjoshi@pop-os.localdomain>
2020-05-25 12:16:50 +0100

    Added styled components and setup global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { unregister } from './core';
+import { GlobalStyles } from './styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>Hello World</div>
+    <>
+      <GlobalStyles />
+      <div>Hello World</div>
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
