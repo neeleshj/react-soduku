f3805ae3a52ced4462c1f03cdf0fc1a9670d082a
Neelesh Joshi <neeleshjoshi@pop-os.localdomain>
2020-05-25 13:26:24 +0100

    Title component added

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom';
 import { unregister } from './core';
 import { GlobalStyles, theme } from './styles';
 import { ThemeProvider } from 'styled-components';
-import { Content } from './components';
+import { Content, Title } from './components';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <Content>
-      <div>Hello World</div>
+    <Content data-cy="content">
+      <Title data-cy="title"> Soduku </Title>
     </Content>
   </ThemeProvider>,
   document.getElementById('root')
