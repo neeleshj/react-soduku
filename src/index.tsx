51c189bbd54bcd54b6cda6a98fa5415e63db753e
Neelesh Joshi <neeleshjoshi@pop-os.localdomain>
2020-05-27 10:04:32 +0100

    Card component created

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom';
 import { unregister } from './core';
 import { GlobalStyles, theme } from './styles';
 import { ThemeProvider } from 'styled-components';
-import { Content, Title } from './components';
+import { Content, Title, Card } from './components';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
     <Content data-cy="content">
       <Title data-cy="title"> Soduku </Title>
+      <Card data-cy="card">WIP</Card>
     </Content>
   </ThemeProvider>,
   document.getElementById('root')
