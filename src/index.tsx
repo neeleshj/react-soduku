839e4c44caa52d0379858a95225b4697c0e30da3
Neelesh Joshi <neeleshjoshi@pop-os.localdomain>
2020-05-31 19:02:35 +0100

    Added the absolute path, started on the logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { unregister } from './core';
-import { GlobalStyles, theme } from './styles';
+import { unregister } from 'core';
+import { GlobalStyles, theme } from 'styles';
 import { ThemeProvider } from 'styled-components';
-import { Content, Title, Card, Grid } from './components';
+import { Content, Title, Card, Grid } from 'components';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
