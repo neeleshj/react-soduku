9bbb2b5da0832c21e71d0f7a19fc2926afc2e4ac
Neelesh Joshi <neeleshjoshi@pop-os.localdomain>
2020-06-08 13:33:49 +0100

    Redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { unregister } from 'core';
+import { Provider } from 'react-redux';
+import { unregister, configureStore } from 'core';
 import { GlobalStyles, theme } from 'styles';
 import { ThemeProvider } from 'styled-components';
 import { Content, Title, Card, Grid } from 'components';
 
+const store = configureStore();
+
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <Content data-cy="content">
-      <Title data-cy="title"> Soduku </Title>
-      <Card data-cy="card">
-        <Grid />
-      </Card>
-    </Content>
+    <Provider store={store}>
+      <Content data-cy="content">
+        <Title data-cy="title"> Soduku </Title>
+        <Card data-cy="card">
+          <Grid />
+        </Card>
+      </Content>
+    </Provider>
   </ThemeProvider>,
   document.getElementById('root')
 );
