2e7970fd1a9d0dbd405cd214eb961fd606e0dc31
Neelesh Joshi <neeleshjoshi@pop-os.localdomain>
2020-06-08 13:41:21 +0100

    Reducers setup

--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -1,10 +1,17 @@
 import { AnyAction } from 'redux';
-import { IReducer } from 'reducers';
+import { IReducer } from './interfaces';
+import * as types from './types';
+import { createFullGrid } from 'utils';
 
-const initialState = {};
+const initialState: IReducer = {};
 
 function reducer(state = initialState, action: AnyAction) {
   switch (action.type) {
+    case types.CREATE_GRID:
+      return {
+        ...state,
+        grid: createFullGrid(),
+      };
     default:
       return state;
   }
