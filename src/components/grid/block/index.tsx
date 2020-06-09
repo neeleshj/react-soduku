d4ed470e995d0077fd33e7202c7460442c3e99a4
Neelesh Joshi <neeleshjoshi@pop-os.localdomain>
2020-06-09 11:43:04 +0100

    Blocks now rendering their values

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -1,15 +1,30 @@
 import React, { FC } from 'react';
 import { Container } from './styles';
 
+import { IReducer } from 'reducers';
+import { useSelector } from 'react-redux';
+import { N } from 'typings';
+
 interface IProps {
   colIndex: number;
   rowIndex: number;
 }
 
+interface IState {
+  value: N;
+}
+
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
+  const state = useSelector<IReducer, IState>(({ grid }) => ({
+    value: grid ? grid[rowIndex][colIndex] : 0,
+  }));
   // Other on the way
 
-  return <Container data-cy={`block-${rowIndex}-${colIndex}`} />;
+  return (
+    <Container data-cy={`block-${rowIndex}-${colIndex}`}>
+      {state.value === 0 ? '' : state.value}
+    </Container>
+  );
 };
 
 export default Block;
