143e9325c8f5d1f1618187eb031a670a70e3d1bd
Neelesh Joshi <neeleshjoshi@pop-os.localdomain>
2020-06-11 15:55:47 +0100

    Can now fill out the board with numbers

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -3,27 +3,46 @@ import { Container, Row } from './styles/index';
 import Block from './block';
 import { useDispatch, useSelector } from 'react-redux';
 import { AnyAction } from 'redux';
-import { createGrid, IReducer, selectBlock } from 'reducers';
-import { INDEX, BLOCK_COORDS } from 'typings';
+import { createGrid, IReducer, selectBlock, fillBlock } from 'reducers';
+import { INDEX, BLOCK_COORDS, NUMBERS, N } from 'typings';
 import useMousetrap from 'react-hook-mousetrap';
 
 interface IState {
   selectedBlock?: BLOCK_COORDS;
+  selectedValue: N;
 }
 
 const Grid: FC = () => {
-  const state = useSelector<IReducer, IState>(({ selectedBlock }) => ({
-    selectedBlock,
-  }));
+  const state = useSelector<IReducer, IState>(
+    ({ selectedBlock, workingGrid }) => ({
+      selectedBlock,
+      selectedValue:
+        workingGrid && selectedBlock
+          ? workingGrid[selectedBlock[0]][selectedBlock[1]]
+          : 0,
+    })
+  );
+
   const dispatch = useDispatch<Dispatch<AnyAction>>();
+
   // Only if dispatch changes, usecallback changes
   const create = useCallback(() => {
     dispatch(createGrid());
   }, [dispatch]);
+
   // Happens on load, create changes -> runs the callback which is dispatch
   useEffect(() => {
     create();
   }, [create]);
+
+  const fill = useCallback(
+    (n: NUMBERS) => {
+      if (state.selectedBlock && state.selectedValue === 0) {
+        dispatch(fillBlock(n, state.selectedBlock));
+      }
+    },
+    [dispatch, state.selectedBlock, state.selectedValue]
+  );
 
   function moveDown() {
     if (state.selectedBlock && state.selectedBlock[0] < 8) {
@@ -68,6 +87,16 @@ const Grid: FC = () => {
       );
     }
   }
+
+  useMousetrap('1', () => fill(1));
+  useMousetrap('2', () => fill(2));
+  useMousetrap('3', () => fill(3));
+  useMousetrap('4', () => fill(4));
+  useMousetrap('5', () => fill(5));
+  useMousetrap('6', () => fill(6));
+  useMousetrap('7', () => fill(7));
+  useMousetrap('8', () => fill(8));
+  useMousetrap('9', () => fill(9));
 
   useMousetrap('down', moveDown);
   useMousetrap('right', moveRight);
